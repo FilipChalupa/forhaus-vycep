d140a159e0a567598fa299cb9344e47aba296fba
Filip Chalupa <chalupafilip@gmail.com>
2013-10-24 11:39:09 +0200

    Get data function

--- media/js/main.js
+++ media/js/main.js
@@ -2,16 +2,25 @@ $(function () {
     var $body = $('body'),
     	$buttons = $('.button'),
     	$menus = $('#top .menu'),
-    	$selection = $('#selection .place');
+    	$selection = $('#selection .place'),
+    	$contents = $('#content > .specific'),
+    	$window = $(window);
     $selection.click(function(){
     	$body.addClass($(this).data('place'));
     });
+    function onResize(){
+    	$contents.css('min-height',$window.height());
+    }
+    $window.resize(function(){
+    	onResize();
+    });
+    onResize();
 	$buttons.on('click',function(event) {
-		var $this = $(this);
-		doAction($this.data('action'));
-		doAction($this.data('secondary'));
+		$.each($(this).data('action').split(';'),function(key,val){
+			doAction(val);
+		});
 	});
-	function doAction(action,action2) {
+	function doAction(action) {
 		switch (action) {
 			case 'menu':
 				$menus.toggleClass('show');
@@ -25,4 +34,44 @@ $(function () {
 				break;
 		}
 	}
+	var storageNames = {
+		'vycepNews': 'http://vycepnastojaka.cz/api/news/',
+		'vycepBeers': 'http://vycepnastojaka.cz/api/beers/',
+		'vycepProducts': 'http://vycepnastojaka.cz/api/products/',
+		//'vycepGalleries': 'http://vycepnastojaka.cz/api/galleries/'
+	};
+	if (!localStorage.lastupdate) {
+		localStorage.lastupdate = 0;
+		$.each(storageNames,function(key,val){
+			localStorage[key] = '[]';
+		});
+	}
+	function updateStorage(sections) {
+		var time = new Date,
+			time = time.getTime(),
+			thisSection = false,
+			newSections = {};
+		$.each(sections,function(key,val){
+			if (thisSection === false) {
+				thisSection = key;
+			} else {
+				newSections[key] = val;
+			}
+		});
+		$.ajax({
+		    url: storageNames[thisSection],
+		    jsonp: "callback",
+		    dataType: "jsonp",
+		    data: {
+		        format: "json"
+		    },
+		    success: function( response ) {
+		    	localStorage[thisSection] = JSON.stringify(response);
+				if (Object.keys(newSections).length !== 0) {
+					updateStorage(newSections);
+				}
+		    }
+		});
+	}
+	updateStorage(storageNames);
 });
