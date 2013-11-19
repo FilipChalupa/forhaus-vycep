311ee7751b25649499570a76a125324772024f9d
Filip Chalupa <chalupafilip@gmail.com>
2013-11-19 20:13:01 +0100

    It was for testing. Now it is gone.

--- media/js/main.js
+++ media/js/main.js
@@ -105,7 +105,6 @@ $(function () {
 	});
     function onResize(){
     	$contents.css('min-height',$window.height());
-    	alert($top.height());
     	$menus.css('height',$window.height()-2*$top.height());
     }
     $window.resize(function(){
